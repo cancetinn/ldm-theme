3f8327f46c86a77941a31cae7b1d216818c46129
Can Çetin <swartzcan@gmail.com>
2024-03-02 14:38:48 +0300

    about us update

--- inc/elements/element-aboutbanner.php
+++ inc/elements/element-aboutbanner.php
@@ -122,67 +122,7 @@ class Aboutbanner extends Widget_Base
                     <div class="textArea">
                         <h1 class="title"><?php echo $settings['big_text']?></h1>
                         <?php echo $settings['banner_content']?>
-                        <div class="abList">
-                            <h4 class="title"><?php echo $settings['list_text']?></h4>
-                            <ul>
-                            <?php
-/*                            foreach ($settings['about_list'] as $item) :
-                                */?><!--
-                                <li><?php /*echo $item['title']; */?></li>
-                            --><?php
-/*
-                            endforeach;
 
-                            */?>
-                                <li>Game Design & Development: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>UI/UX Design:</span> Craft interactive and immersive user interfaces to enhance the gaming experience.</li>
-                                        <li><span>Platform Development:</span> Tailored platforms for an optimized gaming experience.</li>
-                                        <li><span>iOS/Android Game Development:</span> Seamless, engaging games for mobile users.</li>
-                                        <li><span>Unity Game Development:</span> Advanced games utilizing Unity’s powerful engine.</li>
-                                        <li><span>Virtual Reality Game Development:</span> Immersive VR games that transport players to otherworldly realms.</li>
-                                    </ul>
-                                </li>
-                                <li>Gaming Solutions Agency: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>Localization:</span> Adapting games to resonate with diverse cultures and languages.</li>
-                                        <li><span>User Acquisition:</span> Strategies to expand your game’s user base.</li>
-                                        <li><span>Brand Strategy:</span> Crafting a resonant identity for your gaming brand.</li>
-                                        <li><span>Event Planning & Execution:</span> Flawless orchestration of gaming events.</li>
-                                        <li><span>Marketing Strategy:</span> Targeted campaigns to enhance visibility and user engagement.</li>
-                                    </ul>
-                                </li>
-                                <li>Esports & Tournament Operations: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>Community Management:</span> Building and nurturing gaming communities.</li>
-                                        <li><span>Esports Management:</span> Comprehensive management of esports operations.</li>
-                                        <li><span>Event Management:</span> Seamless organization of both online and offline gaming events.</li>
-                                    </ul>
-                                </li>
-                                <li>Broadcast & Production Services:: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>Broadcast Setup & Crew:</span> Full-fledged broadcasting solutions with professional crew members.</li>
-                                        <li><span>Video Production:</span> High-quality video content tailored for gamers.</li>
-                                        <li><span>Online & Offline Production:</span> Events broadcasted and produced for various platforms.</li>
-                                        <li><span>Location Consultancy & Services:</span> Ideal location scouting for offline events.</li>
-                                    </ul>
-                                </li>
-                                <li>Virtual Reality Services: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>VR Training Solutions:</span> Using VR for comprehensive training modules.</li>
-                                        <li><span>Product Demonstration & Installation:</span> Showcasing products in an immersive VR environment.</li>
-                                        <li><span>Marketing Strategies:</span> Innovative VR marketing campaigns.</li>
-                                    </ul>
-                                </li>
-                                <li>Brand Lift Programs: <a href="#" class="toggle-btn">Read More</a>
-                                    <ul class="dropdown-content">
-                                        <li><span>Brand Collaboration:</span> Collaborative efforts to elevate brand visibility.</li>
-                                        <li><span>Licensed 3rd Party Events:</span> Events in collaboration with notable third-party entities.</li>
-                                        <li>Infused with innovation and unwavering passion, we stand at the forefront of the esports industry, spanning tournaments, influencer collaborations, content creation, marketing endeavors, and beyond. Lidoma Vision Esports is unwavering in its dedication to deliver unparalleled gaming experiences, pioneering untrodden paths in the vast expanse of the gaming universe. To tap into the vast reservoir of opportunities we offer and to embark on an unparalleled gaming journey, contact us at info@lidoma.com.</li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                     <div class="imgArea lazyload lazyloaded">
                         <?php echo getImage( $settings['banner_image']['id']  ); ?>
@@ -191,25 +131,95 @@ class Aboutbanner extends Widget_Base
             </div>
         </section>
 
+        <section class="abServices">
+            <div class="container banner">
+                <div class="abList">
+                    <h4 class="title"><?php echo $settings['list_text']?></h4>
+                    <ul>
+                        <li>Game Design & Development: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>UI/UX Design:</span> Craft interactive and immersive user interfaces to enhance the gaming experience.</li>
+                                <li><span>Platform Development:</span> Tailored platforms for an optimized gaming experience.</li>
+                                <li><span>iOS/Android Game Development:</span> Seamless, engaging games for mobile users.</li>
+                                <li><span>Unity Game Development:</span> Advanced games utilizing Unity’s powerful engine.</li>
+                                <li><span>Virtual Reality Game Development:</span> Immersive VR games that transport players to otherworldly realms.</li>
+                            </ul>
+                        </li>
+                        <li>Gaming Solutions Agency: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>Localization:</span> Adapting games to resonate with diverse cultures and languages.</li>
+                                <li><span>User Acquisition:</span> Strategies to expand your game’s user base.</li>
+                                <li><span>Brand Strategy:</span> Crafting a resonant identity for your gaming brand.</li>
+                                <li><span>Event Planning & Execution:</span> Flawless orchestration of gaming events.</li>
+                                <li><span>Marketing Strategy:</span> Targeted campaigns to enhance visibility and user engagement.</li>
+                            </ul>
+                        </li>
+                        <li>Esports & Tournament Operations: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>Community Management:</span> Building and nurturing gaming communities.</li>
+                                <li><span>Esports Management:</span> Comprehensive management of esports operations.</li>
+                                <li><span>Event Management:</span> Seamless organization of both online and offline gaming events.</li>
+                            </ul>
+                        </li>
+                        <li>Broadcast & Production Services:: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>Broadcast Setup & Crew:</span> Full-fledged broadcasting solutions with professional crew members.</li>
+                                <li><span>Video Production:</span> High-quality video content tailored for gamers.</li>
+                                <li><span>Online & Offline Production:</span> Events broadcasted and produced for various platforms.</li>
+                                <li><span>Location Consultancy & Services:</span> Ideal location scouting for offline events.</li>
+                            </ul>
+                        </li>
+                        <li>Virtual Reality Services: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>VR Training Solutions:</span> Using VR for comprehensive training modules.</li>
+                                <li><span>Product Demonstration & Installation:</span> Showcasing products in an immersive VR environment.</li>
+                                <li><span>Marketing Strategies:</span> Innovative VR marketing campaigns.</li>
+                            </ul>
+                        </li>
+                        <li>Brand Lift Programs: <a href="#" class="toggle-btn">Read More</a>
+                            <ul class="dropdown-content">
+                                <li><span>Brand Collaboration:</span> Collaborative efforts to elevate brand visibility.</li>
+                                <li><span>Licensed 3rd Party Events:</span> Events in collaboration with notable third-party entities.</li>
+                                <li>Infused with innovation and unwavering passion, we stand at the forefront of the esports industry, spanning tournaments, influencer collaborations, content creation, marketing endeavors, and beyond. Lidoma Vision Esports is unwavering in its dedication to deliver unparalleled gaming experiences, pioneering untrodden paths in the vast expanse of the gaming universe. To tap into the vast reservoir of opportunities we offer and to embark on an unparalleled gaming journey, contact us at info@lidoma.com.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
         <div class="spacex" style="--spacer:250px"></div>
 
         <script>
-            document.addEventListener('DOMContentLoaded', function () {
+            document.addEventListener('DOMContentLoaded', function() {
                 var toggleButtons = document.querySelectorAll('.toggle-btn');
 
                 toggleButtons.forEach(function(btn) {
                     btn.addEventListener('click', function(e) {
-                        e.preventDefault();
-                        var dropdown = this.nextElementSibling;
-                        if (dropdown.classList.contains('show')) {
-                            dropdown.style.maxHeight = '0';
-                            setTimeout(function() {
-                                dropdown.classList.remove('show');
-                            }, 10);
+                        e.preventDefault(); // Sayfa yenilenmesini önle
+                        var dropdownContent = this.nextElementSibling;
+
+                        if (dropdownContent.classList.contains('show')) {
+                            // Eğer zaten gösteriliyorsa, gizle
+                            dropdownContent.classList.remove('show');
+                            dropdownContent.style.maxHeight = null;
+                            dropdownContent.style.visibility = "hidden";
                             this.textContent = 'Read More';
                         } else {
-                            dropdown.classList.add('show');
-                            dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+                            // Diğer tüm dropdown'ları kapat
+                            document.querySelectorAll('.dropdown-content').forEach(function(el) {
+                                el.classList.remove('show');
+                                el.style.maxHeight = null;
+                                el.style.visibility = "hidden";
+                            });
+                            document.querySelectorAll('.toggle-btn').forEach(function(button) {
+                                button.textContent = 'Read More';
+                            });
+
+                            // Şimdi tıklanan dropdown'ı göster
+                            dropdownContent.classList.add('show');
+                            dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
+                            dropdownContent.style.visibility = "visible";
                             this.textContent = 'Hide';
                         }
                     });
